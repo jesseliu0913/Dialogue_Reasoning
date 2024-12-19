9f19f087deec6f236aa37d315038dbdd8f9bc272
Jesse Liu <112784299+jesseliu0913@users.noreply.github.com>
2024-12-18 21:08:42 -0600

    Update load_model.py

--- pipeline/load_model.py
+++ pipeline/load_model.py
@@ -20,9 +20,10 @@ def init_model(model_name: str, model_args: str, task_name: str, lora_weight=Non
 
     # load the model
     if model_name ==  "hf":
-        model = AutoModelForCausalLM.from_pretrained(model_weight, torch_dtype=torch.bfloat16, device_map="auto", low_cpu_mem_usage=True, trust_remote_code=True)
         if lora_weight != None:
-            model = PeftModel.from_pretrained(model, lora_weight)
+            model = AutoModelForCausalLM.from_pretrained(lora_weight, torch_dtype=torch.bfloat16, device_map="auto", low_cpu_mem_usage=True, trust_remote_code=True)
+        else:
+            model = AutoModelForCausalLM.from_pretrained(model_weight, torch_dtype=torch.bfloat16, device_map="auto", low_cpu_mem_usage=True, trust_remote_code=True)
        
 
     elif model_name == "openai":
